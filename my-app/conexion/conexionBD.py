dc12d0b091c4a9657534a024a8f00e67925ba0a6
sebastianquinllin <sebastian.quinllin@ucuenca.edu.ec>
2024-12-21 11:58:45 -0500

    cambio de datos para la conexion con la base de datos

--- my-app/conexion/conexionBD.py
+++ my-app/conexion/conexionBD.py
@@ -9,11 +9,11 @@ def connectionBD():
     try:
         # connection = mysql.connector.connect(
         connection = mysql.connector.connect(
-            host="monorail.proxy.rlwy.net",
+            host="junction.proxy.rlwy.net",
                 #host="viaduct.proxy.rlwy.net",
-            port=59492,
+            port=48371,
             user="root",
-            passwd="EHFD5A2FFb6h6H-H5e46H2dFBfE1E33D",
+            passwd="YrNMUEvlNKCLkpiRMCjYBXAcASuyyYqL",
                 #passwd="-D2eD6aDb5Bg6dEbhAAeBB6gd3EheaBg",
             database="railway",
                 #database="crud_python",
